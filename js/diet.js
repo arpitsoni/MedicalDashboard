20d76ec35360ce334776b44392ca43e3ddcffe58
Arpit Soni <arpitsoni.it@gmail.com>
2015-04-22 18:24:35 +0530

    Diet Page Recipe Form
    Diet Recipe Form addition+Charts Modification

--- js/diet.js
+++ js/diet.js
@@ -1,7 +1,7 @@
 var DietScript = function () {
 
     //Code to display Weight Line chart information
-    var lineChartData = {
+    var lineChartWeightData = {
         labels : ["Apr 1","Apr 2","Apr 4","Apr 5","Apr 6","Apr 7","Apr 8"],
         datasets : [
             {
@@ -22,8 +22,8 @@ var DietScript = function () {
 
     };
 
-    //Code to display Workout SUmmary Pie chart information
-    var pieData = [
+        //Code to display Workout SUmmary Pie chart information
+    var pieWorkoutSummaryData = [
         {
         value: 24,
         color:"#F7464A",
@@ -45,26 +45,96 @@ var DietScript = function () {
 
     ];
 
+
     //Code to display Workout SUmmary Pie chart information
-    var barChartData = {
-        labels : ["January","February","March","April","May","June","July"],
+    var pieDailyCalorieSummaryData = [
+        {
+        value: 24,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Road Cycling"
+    },
+    {
+        value: 13,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Walking"
+    },
+    {
+        value: 3,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Commuting"
+    }
+
+    ];
+
+    //Code to display Daily Elements Bar chart information
+    var barDailyElementsData = {
+        labels : ["Sodium","Potassium","Calcium","Carbohydrates","Iron","Protein","Fibre"],
         datasets : [
-            {
-                fillColor : "#053A69",
-                strokeColor : "#053A69",
-                data : [65,59,90,81,56,55,40]
+            {   //Values for Individual
+                fillColor : "#46BFBD",
+                strokeColor : "#46BFBD",
+                data : [65,59,90,81,56,55,40],
+                label: "You"
+            },
+            {   //Values by RDA
+                fillColor : "#61E063",
+                strokeColor : "#61E063",
+                data : [65,59,90,81,56,55,40],
+                label: "RDA"
             }
         ]
 
     };
-    
-    var weightLineChart = new Chart(document.getElementById("weightLineChart").getContext("2d")).Line(lineChartData);
-    var dailyCalorieBreakdownBarChart = new Chart(document.getElementById("dailyCalorieBreakdownBarChart").getContext("2d")).Bar(barChartData);
-    var pointsBarChart = new Chart(document.getElementById("pointsBarChart").getContext("2d")).Bar(barChartData);
-    var pieChart = new Chart(document.getElementById("pie").getContext("2d")).Pie(pieData);
-    var dailyCalorieSummaryPieChart = new Chart(document.getElementById("dailyCalorieSummaryPieChart").getContext("2d")).Pie(pieData);
-    $("#pieChartLegends").html(pieChart.generateLegend());
-    $("#dailyCalorieSummaryPieChartLegends").html(pieChart.generateLegend());
+
+    //Code to display Daily Vitamins Bar chart information
+    var barVitaminsData = {
+        labels : ["Vitamin A","Vitamin B1","Vitamin C","Vitamin D","Vitamin E","Vitamin K","Vitamin B2"],
+        datasets : [
+            {   //Values for Individual
+                fillColor : "#46BFBD",
+                strokeColor : "#46BFBD",
+                data : [65,59,90,81,56,55,40],
+                label: "You"
+            },
+            {    //Values by RDA
+                fillColor : "#F476DA",
+                strokeColor : "#F476DA",
+                data : [65,59,90,81,56,55,40],
+                label: "RDA"
+            }
+        ]
+
+    };
+
+    //Create Line chart for displaying Weight information    
+    var weightLineChart = new Chart(document.getElementById("weightLineChart").getContext("2d")).Line(lineChartWeightData);
+
+    //Create Bar chart for displaying Daily Elements information
+    var dailyElementsBarChart = new Chart(document.getElementById("dailyCalorieBreakdownBarChart").getContext("2d")).Bar(barDailyElementsData);
+
+    //Create Bar chart for displaying Daily Vitamins information
+    var dailyVitaminsBarChart = new Chart(document.getElementById("dailyVitaminsBarChart").getContext("2d")).Bar(barVitaminsData);
+
+    //Create Pie chart for displaying Workout Summary information
+    var workoutSummaryPieChart = new Chart(document.getElementById("pie").getContext("2d")).Pie(pieWorkoutSummaryData);
+
+    //Create Line chart for displaying Daily Calorie Summary information
+    var dailyCalorieSummaryPieChart = new Chart(document.getElementById("dailyCalorieSummaryPieChart").getContext("2d")).Pie(pieDailyCalorieSummaryData);
+
+    //Create Legends for Workout Summary Piechart
+    $("#pieChartLegends").html(workoutSummaryPieChart.generateLegend());
+
+    //Create Legends for Daily Calorie Summary Piechart
+    $("#dailyCalorieSummaryPieChartLegends").html(dailyCalorieSummaryPieChart.generateLegend());
+
+     //Create Legends for Daily Elements 
+    $("#dailyElementsBarLegends").html(dailyElementsBarChart.generateLegend());
+
+    //Create Legends for Daily Vitamins
+    $("#dailyVitaminsBarLegends").html(dailyVitaminsBarChart.generateLegend());
 
 }();
 
@@ -170,3 +240,8 @@ var DietEasyPieChart = function () {
             }).data('datepicker');
     });
 
+//wysihtml5 start
+
+$('.wysihtml5').wysihtml5();
+
+//wysihtml5 end
