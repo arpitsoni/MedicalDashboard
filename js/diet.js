1db7edceacbf582d029ebe2f3242b89126f8e119
Archit Soni <archit.cse@live.com>
2015-04-28 23:53:53 +0530

    jQuery RangeSlider added
    jQuery Range Slider added

--- js/diet.js
+++ js/diet.js
@@ -1,3 +1,29 @@
+ //jQuery UI Slider starts
+ $(function() {
+    $( "#prepTime-range-min" ).slider({
+      range: "min",
+      value: 5,
+      min: 1,
+      max: 1440,
+      slide: function( event, ui ) {
+        $( "#prepTime" ).val( ui.value + " min");
+      }
+    });
+    $( "#prepTime" ).val($( "#prepTime-range-min" ).slider( "value" ) + " min");
+
+    $( "#cookTime-range-min" ).slider({
+      range: "min",
+      value: 5,
+      min: 1,
+      max: 1440,
+      slide: function( event, ui ) {
+        $( "#cookTime" ).val( ui.value + " min");
+      }
+    });
+    $( "#cookTime" ).val($( "#cookTime-range-min" ).slider( "value" ) + " min");
+  });
+//jQuery UI Slider ends
+
 var DietScript = function () {
 
     //Code to display Weight Line chart information
